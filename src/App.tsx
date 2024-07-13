17ce0cce173af37ee5a9bdf54d25e6f77a5c58cf
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-07-13 11:20:56 +0500

    Initial commit ✨

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+    return (
+        <div>
+            <Button>Button</Button>
+        </div>
+    );
+};
+
+export default App;
