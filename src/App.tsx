b58b9d55d476c91260eb59fc1a21a29358aef140
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-07-16 11:34:57 +0500

    add: pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SiteFooter from './components/SiteFooter';
 import SiteHeader from './components/SiteHeader';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <>
             <SiteHeader />
+            <Home />
             <SiteFooter />
         </>
     );
