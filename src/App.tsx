900727dcf3142025e231f0e67240b35cd633ca98
comments <frontendmii@gmail.com>
2024-07-20 10:09:04 +0500

    node_modules public src index pacage tailwind.config git pacage-lock .eslintrc .gitignore components pacage postcss.config README tsconfig.app tsconfig tsconfig.node vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import SiteHeader from './components/SiteHeader';
 import SiteFooter from './components/SiteFooter';
 
 const App = () => {
     return (
         <div>
+            <SiteHeader/>
             <SiteFooter/>
         </div>
     );
